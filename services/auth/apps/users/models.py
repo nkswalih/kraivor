fecd3872eb946452ff98aaff6337e1ffa87d99a7
Mohammed Swalih N K <swalink555@gmail.com>
2026-05-08 17:55:12 +0530

    fix: Add app_label to User model Meta class for migration

--- services/auth/apps/users/models.py
+++ services/auth/apps/users/models.py
@@ -49,6 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         db_table = 'auth_users'
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+        app_label = 'users'
 
     def __str__(self):
         return self.email

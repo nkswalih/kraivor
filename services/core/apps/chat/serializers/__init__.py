0ecee94b15b978cc73ab8803fa28c4d88b670624
Mohammed Swalih N K <swalink555@gmail.com>
2026-06-20 09:35:53 +0530

    feat(api): add migration for chat room participant model and last message fields

---
+++ services/core/apps/chat/serializers/__init__.py
@@ -0,0 +1,24 @@
+from apps.chat.serializers.room_serializers import (
+    ChatRoomCreateSerializer,
+    ChatRoomDetailSerializer,
+    ChatRoomListSerializer,
+    ChatRoomUpdateSerializer,
+    CreateDmSerializer,
+)
+from apps.chat.serializers.message_serializers import (
+    MessageSerializer,
+    MessageUpdateSerializer,
+    SendMessageSerializer,
+)
+
+__all__ = [
+    "ChatRoomCreateSerializer",
+    "ChatRoomDetailSerializer",
+    "ChatRoomListSerializer",
+    "ChatRoomUpdateSerializer",
+    "ChatRoomCreateSerializer",
+    "CreateDmSerializer",
+    "MessageSerializer",
+    "MessageUpdateSerializer",
+    "SendMessageSerializer",
+]
---
+++ services/core/apps/chat/views/__init__.py
@@ -0,0 +1,15 @@
+from apps.chat.views.rooms import DMCreateView, RoomDetailView, RoomListCreateView
+from apps.chat.views.messages import (
+    MessageDetailView,
+    MessageListSendView,
+    MessageSearchView,
+)
+
+__all__ = [
+    "DMCreateView",
+    "RoomDetailView",
+    "RoomListCreateView",
+    "MessageDetailView",
+    "MessageListSendView",
+    "MessageSearchView",
+]

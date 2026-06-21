a9a6fcf368c12d093ac756f7d6dafbee2afdbee4
Mohammed Swalih N K <swalink555@gmail.com>
2026-06-21 16:57:16 +0530

    feat(selectors): add RepositorySelector for workspace-related repository queries

--- services/core/apps/repositories/migrations/0002_github_app_models.py
+++ services/core/apps/repositories/migrations/0002_github_app_models.py
@@ -7,7 +7,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [("repositories", "0001_initial"), ("workspaces", "0001_initial")]
 
     operations = [
--- services/core/apps/repositories/migrations/0003_rename_idx_gh_install_workspace_active_idx_gh_install_ws_active.py
+++ services/core/apps/repositories/migrations/0003_rename_idx_gh_install_workspace_active_idx_gh_install_ws_active.py
@@ -4,7 +4,6 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [("repositories", "0002_github_app_models")]
 
     operations = [
